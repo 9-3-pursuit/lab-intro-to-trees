86e69f900cb6a3c40f82d9c3f9f9df06670cab2e
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-07-08 16:24:48 -0400

    finished tree

--- index.js
+++ index.js
@@ -9,8 +9,42 @@ class Tree {
   constructor(node) {
     this.root = node;
   }
-
-  findNode(data) {}
+      /*
+        ┌─A─┐
+      ┌─B   C─┐
+      D       E─┐ 
+                F─┐
+                  G─┐
+                    H─┐
+                      I     
+    */
+   
+  findNode(data, currentNode = this.root) {
+    if (currentNode.data === data) {
+      return currentNode;
+    }
+    for (let child of currentNode.children) {
+      let foundChild = this.findNode(data, child);
+      if (foundChild !== null) {
+        return foundChild;
+      }
+    }
+    return null;
+  }
 }
+
+
+
+// findNode(A)
+// findNode(B)
+// findNode(D)
+// findNode(E)
+// findNode(G)
+// findNode(H)
+
+
+// findNode(B)
+
+// findNode(D)
 
 module.exports = { TreeNode, Tree };
