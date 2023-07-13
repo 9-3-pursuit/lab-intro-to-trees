da5bf24a19fd4388a5fad5fe2442b53bd95eecab
Kerridene Small <Kerridenesmall@gmail.com>
2023-07-13 12:24:15 -0400

    completed lab

--- index.js
+++ index.js
@@ -10,7 +10,37 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    // Start the search from the root node
+    return this.searchNode(this.root, data);
+  }
+
+  searchNode(node, targetNode) {
+    // Check if the current node contains the target data
+    if (node.data === targetNode) {
+      return node;
+    }
+
+    // Recursively search in the children nodes
+    for (const child of node.children) {
+      const result = this.searchNode(child, targetNode);
+      if (result) {
+        return result;
+      }
+    }
+
+    // The target data was not found in the tree
+    return null;
+  }
 }
 
 module.exports = { TreeNode, Tree };
+
+
+
+
+
+
+
+
+
