0f225ec0843e422dbf8b4c8d8cd09732ef483b4a
ryanfodera <ryanfodera@pursuit.org>
2023-07-18 13:24:48 -0400

    All tests passed

--- index.js
+++ index.js
@@ -10,7 +10,35 @@ class Tree {
     this.root = node;
   }
 
-  findNode(data) {}
+  findNode(data) {
+    // Check if the root node matches the specified data
+    if (this.root.data === data) {
+      return this.root;
+    }
+
+    // Helper function to recursively search for the node
+    const searchNode = (node, data) => {
+      // Base case: if the node is null or matches the data, return the node
+      if (node === null || node.data === data) {
+        return node;
+      }
+
+      // Recursively search through each child node
+      for (const child of node.children) {
+        const foundNode = searchNode(child, data);
+        if (foundNode !== null) {
+          return foundNode;
+        }
+      }
+
+      // Node not found
+      return null;
+    };
+
+    // Start the search from the root node
+    return searchNode(this.root, data);
+  }
 }
 
 module.exports = { TreeNode, Tree };
+
